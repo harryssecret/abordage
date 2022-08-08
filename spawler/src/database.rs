0d75e91c13d0f9cf0f5a54bc64d3525a5383b102
harryh <geekcat@protonmail.com>
2022-08-08 23:48:48 +0200

    Moved `Pool` struct into a `Result` enum for better error handling

--- spawler/src/database.rs
+++ spawler/src/database.rs
@@ -1,13 +1,14 @@
 use dotenv::dotenv;
-use sqlx::{postgres::PgPoolOptions, Connection, Pool, Postgres};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use std::env;
 
-pub async fn etablish_connection() -> Pool<Postgres> {
+pub async fn etablish_connection() -> Result<Pool<Postgres>, sqlx::Error> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("Impossible to get environment variable.");
 
-    PgPoolOptions::new()
+    let connection = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
-        .await?
+        .await?;
+    Ok(connection)
 }
