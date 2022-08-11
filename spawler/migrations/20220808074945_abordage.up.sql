bb1b2e4ce5f8f9b66225d154f86e8d1435eaecb3
harryh <geekcat@protonmail.com>
2022-08-11 22:28:25 +0200

    Added migrations scripts

--- spawler/migrations/20220808074945_abordage.up.sql
+++ spawler/migrations/20220808074945_abordage.up.sql
@@ -1,1 +1,16 @@
 -- Add up migration script here
+CREATE TABLE caches {
+    id serial primary key,
+    cache_name text not null,
+    coordinate_x point not null,
+    coordinate_y point not null,
+    difficulty smallint,
+    maintainer varchar(48) references users(username)
+}
+
+CREATE TABLE users {
+    id SERIAL PRIMARY KEY,
+    username varchar(48) NOT NULL,
+    display_name TEXT,
+    gamepoints INTEGER
+}
