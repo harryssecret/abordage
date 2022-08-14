aa81b42e06b2c0c377769e239730bc80083f5fd2
harryh <geekcat@protonmail.com>
2022-08-14 12:29:15 +0200

    Added `UNIQUE` constraint to username row

--- spawler/migrations/20220808074945_abordage.up.sql
+++ spawler/migrations/20220808074945_abordage.up.sql
@@ -1,16 +1,15 @@
--- Add up migration script here
-CREATE TABLE caches {
-    id serial primary key,
-    cache_name text not null,
-    coordinate_x point not null,
-    coordinate_y point not null,
-    difficulty smallint,
-    maintainer varchar(48) references users(username)
-}
-
-CREATE TABLE users {
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username varchar(48) NOT NULL,
+    username varchar(48) UNIQUE NOT NULL,
     display_name TEXT,
     gamepoints INTEGER
-}
+);
+
+CREATE TABLE caches (
+    id serial primary key,
+    cache_name text not null,
+    maintainer varchar(48) references users(username),
+    coordinate_x point not null,
+    coordinate_y point not null,
+    difficulty smallint
+);
