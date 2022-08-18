1251796d5e18c0832ddfe6d6937f53362bf8fe1d
harryh <geekcat@protonmail.com>
2022-08-18 23:01:59 +0200

    Attached migrations and `PGDb` fairing to the rocket build instead of separate ignite instances

--- spawler/src/main.rs
+++ spawler/src/main.rs
@@ -1,10 +1,13 @@
+use rocket::{
+    fairing::{self, AdHoc},
+    Build, Rocket,
+};
 use rocket_db_pools::{sqlx, Database};
 
 #[macro_use]
 extern crate rocket;
 
-mod geocache;
-mod users;
+pub mod types;
 
 #[derive(Database)]
 #[database("abordage")]
@@ -12,5 +15,22 @@ struct PGDb(sqlx::PgPool);
 
 #[launch]
 async fn rocket() -> _ {
-    rocket::build().attach(geocache::register())
+    rocket::build()
+        .attach(PGDb::init())
+        .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
+        .attach(types::geocache::register())
+        .attach(types::users::register())
+}
+
+async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+    match PGDb::fetch(&rocket) {
+        Some(db) => match sqlx::migrate!("./migrations").run(&**db).await {
+            Ok(_) => Ok(rocket),
+            Err(e) => {
+                error!("Impossible to initialize the database : {}", e);
+                Err(rocket)
+            }
+        },
+        None => Err(rocket),
+    }
 }
