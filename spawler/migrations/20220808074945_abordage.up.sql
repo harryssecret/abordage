966c216f5ba28fbd1340d3f61ac78d1cc5ffcd24
harryh <geekcat@protonmail.com>
2022-10-12 14:21:18 +0200

    Replaced first migration since it sucked

--- spawler/migrations/20220808074945_abordage.up.sql
+++ spawler/migrations/20220808074945_abordage.up.sql
@@ -1,4 +1,6 @@
-CREATE TYPE cache_status AS ENUM ('archived', 'maintenance', 'active');
+CREATE EXTENSION Postgis;
+
+CREATE TYPE cache_status AS ENUM ('Archived', 'Maintenance', 'Active');
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -12,9 +14,10 @@ CREATE TABLE users (
 CREATE TABLE caches (
     id serial primary key,
     cache_name text not null,
+    cache_description jsonb,
     maintainer varchar(48) not null references users(username),
-    coordinate_x point not null,
+    cache_location geography(POINT, 4326) not null,
     coordinate_y point not null,
-    difficulty smallint not null CHECK (difficulty > 0 and difficulty < 10),
-    archived_state cache_status not null default 'active'
+    difficulty smallint not null CHECK (difficulty >= 0 and difficulty < 10),
+    archived_state cache_status not null default 'Active'
 );
