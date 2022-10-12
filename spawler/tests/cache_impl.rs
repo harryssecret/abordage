1ccd445cb6641e366f30029825b0f5c3970133f6
harryh <geekcat@protonmail.com>
2022-10-12 14:22:23 +0200

    Started writing tests

--- spawler/tests/cache_impl.rs
+++ spawler/tests/cache_impl.rs
@@ -1,7 +1,14 @@
 #[cfg(test)]
 mod tests {
+    use dotenvy::dotenv;
+    use sqlx::{Connection, PgConnection};
     #[test]
-    fn query_cache() {}
+    fn add_caches_to_db() {
+        dotenv().ok();
+        let database_url =
+            std::env::var("DATABASE_URL").expect("Impossible to parse DATABASE_URL.");
+        let conn = PgConnection::connect(&database_url);
+    }
 
     #[test]
     fn check_routes() {}
