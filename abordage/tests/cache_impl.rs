cded829fd848f00050aed93933fbb22d29f5aa56
harryh <geekcat@protonmail.com>
2022-10-17 22:44:12 +0200

    Started writing tests correctly

--- abordage/tests/cache_impl.rs
+++ abordage/tests/cache_impl.rs
@@ -1,13 +1,11 @@
 #[cfg(test)]
 mod tests {
-    use dotenvy::dotenv;
-    use sqlx::{Connection, PgConnection};
-    #[test]
-    fn add_caches_to_db() {
-        dotenv().ok();
-        let database_url =
-            std::env::var("DATABASE_URL").expect("Impossible to parse DATABASE_URL.");
-        let conn = PgConnection::connect(&database_url);
+    use sqlx::PgPool;
+
+    #[sqlx::test(migrations = "./migrations")]
+    async fn add_caches_to_db(pool: PgPool) -> sqlx::Result<()> {
+        let mut conn = pool.acquire().await?;
+        Ok(())
     }
 
     #[test]
