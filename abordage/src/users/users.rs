388a48d048f9468f7983e2b59b68c98fd5cf8893
harryh <geekcat@protonmail.com>
2022-10-19 23:59:19 +0200

    Routes for handling struct `Pirate`

--- abordage/src/users/users.rs
+++ abordage/src/users/users.rs
@@ -1,4 +1,26 @@
+use std::sync::Arc;
+
+use axum::{extract::Path, Extension, Form};
 use serde::{Deserialize, Serialize};
+
+use crate::AppContext;
+
+async fn create_user(Form(user): Form<NewPirate>, Extension(ctx): Extension<Arc<AppContext>>) {
+    sqlx::query!(
+        r#"INSERT INTO users (username, pirate_password) VALUES ($1, $2)"#,
+        user.username,
+        user.password
+    )
+    .execute(&ctx.db)
+    .await
+    .expect("Impossible to insert new user.");
+}
+
+async fn edit_user(Extension(ctx): Extension<Arc<AppContext>>) {}
+
+async fn delete_user(Extension(ctx): Extension<Arc<AppContext>>) {}
+
+async fn get_user(Path(user_id): Path<i32>, Extension(ctx): Extension<Arc<AppContext>>) {}
 
 #[derive(Serialize, Deserialize)]
 struct NewPirate {
